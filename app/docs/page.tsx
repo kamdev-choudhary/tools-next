7d3daa2675216af34abda4fd2657d653512c3390
Kamdev choudhary <kamdevchoudhary@gmail.com>
2024-12-21 11:57:59 +0530

    updated

---
+++ app/docs/page.tsx
@@ -0,0 +1,11 @@
+import { title } from "@/components/primitives";
+import { Button } from "@nextui-org/button";
+
+export default function DocsPage() {
+  return (
+    <div>
+      <h1 className={title()}>Docs</h1>
+      <Button>Hello</Button>
+    </div>
+  );
+}
